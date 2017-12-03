f4bb4043bd2996fa4169364e43879db259cdb887
Subraiz Ahmed <subraizahmed@gmail.com>
2017-12-02 19:39:16 -0500

    Long Day of Work
    Efficiency at O(n)

--- MinPQ.java
+++ MinPQ.java
@@ -1,47 +1,72 @@
-public class linkedheap{
+public class MinPQ{
 
   private class Node {
     private Node parent;
     private Node leftChild;
     private Node rightChild;
     private int info;
+    private int id;
 
-    Node(int info) {
+    Node(int info, int id) {
         this.info = info;
         this.parent = null;
         this.rightChild = null;
         this.leftChild = null;
+        this.id = id;
     }
   }
 
   private Node root; // Keeps track of the top most Node in the tree.
   private Node last; // Keeps track of the most current Node added.
   int N = 0;
-  int depth = 1;
+  private int counter;
 
 
-  public linkedheap(){ // Constructor function.
+  public MinPQ(){ // Constructor function.
     root = null;
-    last = root;
   }
 
   public boolean isEmpty() {
     return N == 0;
   }
 
-  public int size(){
-    return N;
-  }
-
   public void insert(int info){
-    Node temp = new Node(info);
+    Node temp = new Node(info, N+1);
     if (this.isEmpty()) { // First item is always the root.
       this.root = temp;
     }
     else{
-      balance(temp, root, N, depth); // This function makes sure the tree is balanced.
+      balance(temp, root); // This function makes sure the tree is balanced.
     }
     N++;
+  }
+
+  public void balance(Node key, Node target){ // Assign each Node an id corresponding to where it should be in an array.
+
+    if (key.id == 2*target.id || (key.id == (2*target.id + 1))){
+      if (key.id == 2*target.id){
+        target.leftChild = key;
+        key.parent = target;
+        last = key;
+      }
+
+      else {
+        target.rightChild = key;
+        key.parent = target;
+        last = key;
+      }
+
+      if (key.info < target.info) {
+        swim(key);
+        return;
+      }
+    }
+
+    else if (target.leftChild == null || target.rightChild == null) return; // Recursively go through the linked list until the right Node is found.
+    else {
+      balance(key, target.leftChild);
+      balance(key, target.rightChild);
+    }
   }
 
   public void show(){
@@ -73,20 +98,13 @@ public class linkedheap{
     sink(root);
   }
 
+  public int size(){
+    return N;
+  }
+
+
 //-----------------------------Helper Functions---------------------------------//
 
-  public void balance(Node key, Node target, int size, int depth){ // Assign each Node an id corresponding to where it should be in an array.
-    if (target == null) {key = target;}
-    if ((size + 1) == Math.pow(2, depth)){depth++;}
-    else {
-      if ((Math.pow(2, depth) - size - 1) > Math.pow(depth - 2, 2)){
-        balance(key, target.leftChild, Math.ceil(size/2), (depth - 1));
-      }
-      else if ((Math.pow(2, depth) - size - 1) <= Math.pow(depth - 2, 2)){
-        balance(key, target.rightChild, Math.ceil(size/2), (depth - 1));
-      }
-    }
-  }
 
   private void show(Node toShow){ // Just a simple Pre Order Traversal.
     if (toShow == null) return;
@@ -141,20 +159,16 @@ public class linkedheap{
 //------------------------------------------------------------------------------//
 
   public static void main(String[] args) {
-    linkedheap heap = new linkedheap();
+    MinPQ heap = new MinPQ();
 
     for (int n = 1; n < 20; n++) {
       heap.insert(n);
     }
 
+    System.out.println(heap.size());
     heap.show();
 
     // Test Cases
-    System.out.println();
-    System.out.println("The size of the heap is: " + heap.size());
-    System.out.println("The minimum value is: " + heap.min());
-    System.out.println("Is the heap empty: " + heap.isEmpty());
-    System.out.println();
 
 
 
